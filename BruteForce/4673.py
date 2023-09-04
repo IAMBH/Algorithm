a2415ccd0102ff95ac35f386c6468fcc16607c73
IAMBH <xz7840@dgu.edu>
2023-09-04 17:18:08 +0900

    4673

--- BruteForce/4673.py
+++ BruteForce/4673.py
@@ -1,5 +1,5 @@
 # 셀프 넘버
-# 1~10000 돌면서 각 숫자에 대해 다시 1부터 돌면서 생성자가 있는지 확인
+# 1~10000 돌면서 각 숫자에 대해 리스트에서 제거
 
 # 1~10000
 li = [i for i in range(1,10001)]
@@ -9,10 +9,11 @@ for num in range(1, 10001):
     const = num
     for d in range(len(str(num))):
         const += int(str(num)[d])
-    
+    # 리스트에 숫자가 있으면 제거 
     if const in li:
         li.remove(const)
         
+# 남은 숫자들 출력    
 for num in li:
     print(num)
 
